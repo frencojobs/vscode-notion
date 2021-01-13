69a337285514a2a1ad916e8da9ba6feaf6f020a7
Frenco <hey@frenco.dev>
2021-01-13 20:08:30 +0630

    feat: use theme icon instead of svg

--- src/features/NotionItem.ts
+++ src/features/NotionItem.ts
@@ -1,16 +1,7 @@
-import * as path from 'path'
 import * as vscode from 'vscode'
 
 export default class NotionPageItem extends vscode.TreeItem {
-  iconPath = path.join(
-    __filename,
-    '..',
-    '..',
-    'resources',
-    'icons',
-    'symbol-file.svg'
-  )
-
+  iconPath = new vscode.ThemeIcon('symbol-file')
   command: vscode.Command = {
     title: 'Open Page',
     command: 'vscode-notion.open',
